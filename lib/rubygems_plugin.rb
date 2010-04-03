539bc2807e5bed8fa2d73f59c1269dbc62d2862b
Sven Fuchs <svenfuchs@artweb-design.de>
2010-04-03 02:32:19 +0200

    remove the gem file after pushing it to github. add some output.

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -7,32 +7,39 @@ class Gem::Commands::ReleaseCommand < Gem::Command
     super 'release', 'Build a gem from a gemspec and push to rubygems.org'
   end
 
-  def arguments # :nodoc:
+  def arguments
     "GEMSPEC_FILE optional (will use the first *.gemspec if not specified)"
   end
 
-  def usage # :nodoc:
+  def usage
     "#{program_name} [GEMSPEC_FILE]"
   end
-  
+
   def execute
-    file_name = build
-    push(file_name)
+    filename = build
+    push(filename)
+    remove(filename)
+    say "All done, thanks buddy."
   end
-  
+
   def build
     command = Gem::Commands::BuildCommand.new
     command.handle_options([gemspec])
     command.execute
-    command.load_gemspecs(gemspec).first.file_name
+    command.load_gemspecs(gemspec).first.filename
   end
-  
+
   def push(filename)
     command = Gem::Commands::PushCommand.new
     command.handle_options([filename])
     command.execute
   end
-  
+
+  def remove(filename)
+    `rm #{filename}`
+    say "Deleting left over gem file #{filename}"
+  end
+
   def gemspec
     @gemspec ||= begin
       gemspec = Array(options[:args]).first
