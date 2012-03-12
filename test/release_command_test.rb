4742bceaf6fd05bcccde81528c5701f4ce6e0d71
Dan Gebhardt <dan@cerebris.com>
2012-03-12 00:20:40 -0400

    use bootstrapped gems in release command tests

--- test/release_command_test.rb
+++ test/release_command_test.rb
@@ -1,30 +1,40 @@
 require File.expand_path('../test_helper', __FILE__)
+require 'rubygems/commands/bootstrap_command'
 require 'rubygems/commands/release_command'
 
 class ReleaseCommandTest < Test::Unit::TestCase
   include Gem::Commands
 
   def setup
+    build_sandbox(:gemspec_dirs => true)
+    stub_command(BootstrapCommand, :say)
     stub_command(BuildCommand, :execute)
     stub_command(PushCommand, :execute)
     stub_command(TagCommand, :execute)
     stub_command(ReleaseCommand, :remove, :say)
-    GemRelease::VERSION.replace('0.0.1')
+    BootstrapCommand.new.send(:write_scaffold)
+  end
+
+  def teardown
+    teardown_sandbox
   end
 
   test "build executes BuildCommand with the current gemspec filename" do
-    command = BuildCommand.new
-    command.expects(:execute)
-    ReleaseCommand.new.send(:build)
-    assert_equal ['gem-release.gemspec'], command.options[:args]
+    in_gemspec_dirs do
+      command = BuildCommand.new
+      command.expects(:execute)
+      ReleaseCommand.new.send(:build)
+      assert_equal [Dir['*.gemspec'].first], command.options[:args]
+    end
   end
 
   test "push executes PushCommand with the current gem filename" do
-    command = PushCommand.new
-    command.stubs(:gem_filename).returns('gem-release-0.0.1.gem')
-    command.expects(:execute)
-    ReleaseCommand.new.send(:push)
-    assert_equal ['gem-release-0.0.1.gem'], command.options[:args]
+    in_gemspec_dirs do
+      command = PushCommand.new
+      command.expects(:execute)
+      ReleaseCommand.new.send(:push)
+      assert command.options[:args][0].include?(".gem")
+    end
   end
 
   test "tag executes TagCommand if --tag option was given" do
@@ -33,13 +43,18 @@ class ReleaseCommandTest < Test::Unit::TestCase
   end
 
   test "passes --key args to the push command" do
-    PushCommand.new.expects(:invoke).with("gem-release-0.0.1.gem", "--key", "engineyard")
-    ReleaseCommand.new.invoke('--key', 'engineyard')
+    key_name = "example"
+    in_gemspec_dirs do
+      PushCommand.any_instance.expects(:invoke).with() { |_, a1, a2| a1 ==  "--key" && a2 == key_name }
+    end
+    ReleaseCommand.new.invoke('--key', key_name)
   end
 
   test "passes --host args to the push command" do
-    PushCommand.new.expects(:invoke).with("gem-release-0.0.1.gem", "--host", "http://rubygems.engineyard.com")
-    ReleaseCommand.new.invoke('--host', 'http://rubygems.engineyard.com')
+    host_name = "http://hostname.example.com"
+    in_gemspec_dirs do
+      PushCommand.any_instance.expects(:invoke).with() { |_, a1, a2| a1 ==  "--host" && a2 == host_name }
+    end
+    ReleaseCommand.new.invoke('--host', host_name)
   end
-
 end
