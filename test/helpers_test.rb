a6759a52e0212862780109966639ae7aac00967f
Zander Hill <Zander@civet.ws>
2014-05-11 19:50:09 -0400

    Remove duplication with setup block in helpers_test
    Extract `ReleaseCommand.new` into a `setup` block to remove duplication.

--- test/helpers_test.rb
+++ test/helpers_test.rb
@@ -11,22 +11,21 @@ class HelpersTest < Test::Unit::TestCase
   end
 
   test "gems_filename" do
-    assert_match /gem-release-\d+\.\d+\.\d+\.gem/, ReleaseCommand.new.gem_filename
+    assert_match /gem-release-\d+\.\d+\.\d+\.gem/, command.gem_filename
   end
 
   test "gems_version" do
-    assert_match /^\d+\.\d+\.\d+$/, ReleaseCommand.new.gem_version
+    assert_match /^\d+\.\d+\.\d+$/, command.gem_version
   end
 
   test "gemspec_filename with an cmdline argument given" do
-    command = ReleaseCommand.new
     command.handle_options(['path/to/some.gemspec'])
     assert_equal 'path/to/some.gemspec', command.gemspec_filename
   end
 
   test "gemspec_filename with no cmdline argument given" do
     Dir.stubs(:[]).returns(['some.gemspec'])
-    assert_equal 'some.gemspec', ReleaseCommand.new.gemspec_filename
+    assert_equal 'some.gemspec', command.gemspec_filename
   end
 
   test "rejects gemspecs located in vendor directory" do
