1bdaa73f4c6833a63c64ab5e1229ee91c63b1290
PikachuEXE <pikachuexe@gmail.com>
2019-08-14 13:19:37 +0800

    * Rescue Abort error with error message printed

--- lib/gem/release/support/gem_command.rb
+++ lib/gem/release/support/gem_command.rb
@@ -52,8 +52,8 @@ module Gem
 
       def execute
         Cmds::Runner.new(cmd, opts.delete(:args), opts).run
-      rescue Abort
-        abort
+      rescue Abort => ex
+        abort(ex.message)
       end
 
       def cmd
