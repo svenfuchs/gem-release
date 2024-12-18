dc0b3ce4437f797a095a255595fc6003cacdb939
PikachuEXE <git@pikachuexe.net>
2024-12-18 10:25:50 +0800

    ! Fix error seen when this gem installed but not specified as dependency in project

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,14 +1,18 @@
-require 'gem/release'
-require 'rubygems/command_manager'
+begin
+  require 'gem/release'
+  require 'rubygems/command_manager'
 
-require 'rubygems/commands/bootstrap_command'
-require 'rubygems/commands/bump_command'
-require 'rubygems/commands/gemspec_command'
-require 'rubygems/commands/release_command'
-require 'rubygems/commands/tag_command'
+  require 'rubygems/commands/bootstrap_command'
+  require 'rubygems/commands/bump_command'
+  require 'rubygems/commands/gemspec_command'
+  require 'rubygems/commands/release_command'
+  require 'rubygems/commands/tag_command'
 
-Gem::CommandManager.instance.register_command :bootstrap
-Gem::CommandManager.instance.register_command :bump
-Gem::CommandManager.instance.register_command :gemspec
-Gem::CommandManager.instance.register_command :release
-Gem::CommandManager.instance.register_command :tag
+  Gem::CommandManager.instance.register_command :bootstrap
+  Gem::CommandManager.instance.register_command :bump
+  Gem::CommandManager.instance.register_command :gemspec
+  Gem::CommandManager.instance.register_command :release
+  Gem::CommandManager.instance.register_command :tag
+rescue LoadError
+  # Plugin is sometimes run even on projects without this gem specified as dependency
+end
